c62e4035db4aadbfab430431bd98a4c1e03ea446
David Albornoz <albornozd@usf.edu>
2026-02-15 19:58:30 -0500

    created array of objects

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+let products = [
+  { name: "Airpods", category: "electronics", price: 129.99, inventory: 10 },
+  { name: "Jacket", category: "apparel", price: 49.99, inventory: 8 },
+  { name: "Rice", category: "groceries", price: 7.99, inventory: 25 },
+  { name: "All-Purpose Cleaner", category: "household", price: 12.99, inventory: 12 },
+  { name: "Rope", category: "outdoors", price: 15.0, inventory: 6 }
+];
+
+console.log(products);
